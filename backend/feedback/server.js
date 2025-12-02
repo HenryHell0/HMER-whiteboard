b674cee6b89ee9f2dcc19cdf4ed954c7308f758d
Henry Holton <noway1526@gmail.com>
2025-12-02 00:13:59 -0500

    add backend for /feedback/ api endpoint for getting user feedback

---
+++ backend/feedback/server.js
@@ -0,0 +1,49 @@
+import express from "express"
+import path from "path"
+import fs from "fs"
+import cors from "cors"
+import { cwd } from "process"
+
+const app = express()
+app.use(express.json())
+app.use(
+	cors({
+		origin: "https://henryhell0.github.io",
+	})
+)
+
+const filePath = cwd() + "/data/feedback.txt"
+
+app.post("/feedback", (request, response) => {
+	if (!request.body?.message || !request.body?.userInfo) {
+		return response.status(400).json({ error: "Invalid input" })
+	}
+
+	const entry = {
+		message: request.body.message,
+		userInfo: request.body.userInfo,
+		createdDate: new Date().toLocaleString("en-GB"),
+	}
+
+	// let existing = [];
+	// if (fs.existsSync(filePath) && fs.statSync(filePath).size != 0) {
+	//     existing = JSON.parse(fs.readFileSync(filePath, "utf8"))
+	// }
+
+	// existing.push(entry)
+
+	const content = `User: ${entry.userInfo}\nMessage: ${entry.message}\nDate: ${entry.createdDate} \n\n\n`
+	console.log(content)
+	fs.appendFile(filePath, content, (error) => {
+		if (error) {
+			console.error("Write failed:", error)
+			return response.status(500).json({ status: "error" })
+		}
+	})
+
+	response.json({ status: "ok" })
+})
+
+app.listen(3100, () => {
+	console.log("Post API running on 3100!! :)")
+})
